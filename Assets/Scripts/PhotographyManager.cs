690f4528d34d76a786795a64f7639d0b91ed18b2
Matthew Conto <mbsconto@gmail.com>
2015-04-15 15:59:09 -0400

    Managers

---
+++ Assets/Scripts/PhotographyManager.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class PhotographyManager : MonoBehaviour {
+
+//	public List<
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
