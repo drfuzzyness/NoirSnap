1cf3535950a9fcddbb5bcf5562ece3194c4c215e
Rich Campbell <rich.campbell609@gmail.com>
2015-04-15 12:40:12 -0400

    my player controller, with a nice pink floor and baby blue buildings

---
+++ Assets/Scripts/player_controller.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class player_controller : MonoBehaviour {
+	//might not need this
+	public Transform player;
+
+	public float playerSpeed;
+	public float turnSpeed;
+	
+	void Update () {
+		if(Input.GetKey(KeyCode.W)){
+			GetComponent<Rigidbody>().AddForce(transform.forward * playerSpeed * Time.deltaTime, ForceMode.VelocityChange);
+		}
+		if(Input.GetKey(KeyCode.S)){
+			GetComponent<Rigidbody>().AddForce(transform.forward * -playerSpeed * Time.deltaTime, ForceMode.VelocityChange);
+		}
+		if(Input.GetKey(KeyCode.A)){
+			transform.Rotate(0f,-90f * turnSpeed * Time.deltaTime,0f);
+		}
+		if(Input.GetKey(KeyCode.D)){
+			transform.Rotate(0f,90f * turnSpeed * Time.deltaTime,0f);
+		}
+	
+	}
+}
