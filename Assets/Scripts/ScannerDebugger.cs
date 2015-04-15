f7446b92a4a2fe033b39f3fae40276f98216842e
Matthew Conto <mbsconto@gmail.com>
2015-04-15 13:59:07 -0400

    Working on Scan Debugger

---
+++ Assets/Scripts/ScannerDebugger.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent (typeof (ScanForTarget) )]
+public class ScannerDebugger : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if( GetComponent<ScanForTarget>().sees( GetComponent<ScanForTarget>().player.gameObject ) ) {
+			Debug.Log ( gameObject + " sees" );
+		}
+	}
+}
