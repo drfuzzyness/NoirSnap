38fdd4b621ff12c9306b91bb3a2ea3319af2238a
Matthew Conto <mbsconto@gmail.com>
2015-04-19 16:07:58 -0400

    Working on Dialog

--- Assets/Scripts/Text/Dialogue.cs
+++ Assets/Scripts/Text/Dialogue.cs
@@ -1,7 +1,14 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
+using System.Collections.Generic;
+
 
 public class Dialogue : MonoBehaviour {
+
+	public Text outputStream;
+	public List<string> lines;
+	public List<float> delays;
 
 	// Use this for initialization
 	void Start () {
