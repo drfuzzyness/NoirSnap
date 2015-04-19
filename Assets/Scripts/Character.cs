eb02c7024b21f6d650bd0eaa039b3e6174311692
Matthew Conto <mbsconto@gmail.com>
2015-04-19 00:10:42 -0400

    Camera...

---
+++ Assets/Scripts/Character.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+// Every character in the game is of type Character. This has to due with determining innocence
+public class Character : MonoBehaviour {
+	
+	[Header("State")]
+	public bool oppinionOnInnocence = false; // Does the player think this person can be completly innocent
+
+	[Header("Balance")]
+	public bool innocent = true;
+
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/Scripts/Damageable.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+//[RequireComponent (typeof (Character)) ]
+// Things that aren't characters could be killable. Like shooting pidgeons or doves.
+public class Damageable : MonoBehaviour {
+
+	[Header("Status")]
+	public float health; // summary of current health including diet, cancers, physical injury
+	public bool alive = true; // possibly innacurate simulation of life. can things be half-dead?
+
+	[Header("Balance")]
+	public float startingHealth = 10f; // mostly genetic, partially based on "free will" of the character.
+	public bool isCharacter; // check if the damageable object is a character
+
+
+	
+	public void kill() { // is kill
+		
+	}
+
+	public void damage( float dmg ) {
+		health -= dmg;
+		if( !isAlive() )
+			kill();
+	}
+
+	public bool isAlive() {
+		return health > 0;
+	}
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
