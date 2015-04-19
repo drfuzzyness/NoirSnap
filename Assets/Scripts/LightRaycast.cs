36222e0d9ebf8f2b43a20eb5f3aa58ad6e8db873
Shaun Bandrowski <shaun.toro@gmail.com>
2015-04-19 16:07:30 -0400

    LightRaycast Stuff

---
+++ Assets/Scripts/LightRaycast.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LightRaycast : MonoBehaviour {
+
+	public Transform playerPrefab;
+
+	// Use this for initialization
+	void Start () {
+
+
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		Ray lightRay = new Ray(transform.position, transform.forward);
+
+		RaycastHit[] obstacles = Physics.SphereCastAll (lightRay, 2.3f,50f);
+
+		for (int i = 0; i < obstacles.Length; i++){
+
+			if (obstacles[i].collider.tag == "Player"){
+
+				playerPrefab.GetComponent<StealthPercent>().stealth = false;
+				break;
+
+			}
+			else{
+				
+				playerPrefab.GetComponent<StealthPercent>().stealth = true;
+				
+			}
+
+		}
+//		if (Physics.SphereCast(lightRay, 3f,out rayHit, 50f) && rayHit.collider.tag == "Player"){
+//
+//			playerPrefab.GetComponent<StealthPercent>().stealth = false;
+//			
+//		}
+//		else{
+//
+//			playerPrefab.GetComponent<StealthPercent>().stealth = true;
+//
+//		}
+	
+	}
+	
+}
--- Assets/Scripts/playerControl.cs
+++ Assets/Scripts/playerControl.cs
@@ -4,11 +4,8 @@ using System.Collections;
 public class playerControl : MonoBehaviour {
 
 	public float speed = 5f;
-	public float sneakSpeed = 5f;
 	public float turnSpeed = 5f;
-	// public bool isSneaking = false;
 
-	
 
 	Rigidbody rbody;
 	// Use this for initialization
@@ -21,17 +18,10 @@ public class playerControl : MonoBehaviour {
 	// Update is called once per frame
 	void FixedUpdate () {
 
+
 		rbody.AddForce (transform.forward * speed * Input.GetAxis("Vertical"));
 		transform.Rotate (0f, Input.GetAxis ("Horizontal") * turnSpeed, 0f);
-
-		if (Input.GetKey(KeyCode.LeftShift)){
-
-			rbody.AddForce (transform.forward * sneakSpeed * Input.GetAxis("Vertical"));
-
-
-		}
-
-
+	
 
 	}
 
