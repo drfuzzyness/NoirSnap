8ab91e1903e68fb6e1794c71815e963399fa9a68
Shaun Bandrowski <shaun.toro@gmail.com>
2015-04-22 14:24:28 -0400

    Made itemList, added inBox bool to Hidden.cs.

--- Assets/Scripts/LightRaycast.cs
+++ Assets/Scripts/LightRaycast.cs
@@ -4,12 +4,12 @@ using System.Collections;
 public class LightRaycast : MonoBehaviour {
 
 	public Transform playerPrefab;
+	public Transform boxPrefab;
 
 	// Use this for initialization
 	void Start () {
 
 		playerPrefab.GetComponent<StealthPercent>().stealth = true;
-
 	
 	}
 	
@@ -19,6 +19,8 @@ public class LightRaycast : MonoBehaviour {
 		// ray from spotlight to the ground
 		Ray lightRay = new Ray(transform.position, transform.forward);
 		// gets a list of all the colliders the light ray is hitting
+		// second and third parameters are tuned for a spotlight with a range of 50, spot angle of 50, and height of 5.35
+		// Tune those parameters to fit the spotlight you make best
 		RaycastHit[] obstacles = Physics.SphereCastAll (lightRay, 2.3f,50f);
 		// goes through the list of colliders
 		for (int i = 0; i < obstacles.Length; i++){
@@ -26,13 +28,20 @@ public class LightRaycast : MonoBehaviour {
 			if (obstacles[i].collider.tag == "Player"){
 				// if one of them is the player set the stealth status to false
 				playerPrefab.GetComponent<StealthPercent>().stealth = false;
+				// HOWEVER, if the player is currently inside of a box then, they are considered HIDDEN even though they're in the spotlight's raycast
+				if (boxPrefab.GetComponent<Hidden>().inBox == true){
+					
+					playerPrefab.GetComponent<StealthPercent>().stealth = true;
+					
+				}
 				break;
 
 			}
-//			else{
-//				playerPrefab.GetComponent<StealthPercent>().stealth = true;
-//
-//			}
+
+			else{
+				playerPrefab.GetComponent<StealthPercent>().stealth = true;
+
+			}
 
 
 
