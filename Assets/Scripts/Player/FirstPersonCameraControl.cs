725db701e5cdc2dd2f1420fa68e95ee13a7b1ade
Matthew Conto <mbsconto@gmail.com>
2015-04-26 16:57:07 -0400

    Fixed Camera issues

--- Assets/Scripts/Player/FirstPersonCameraControl.cs
+++ Assets/Scripts/Player/FirstPersonCameraControl.cs
@@ -6,6 +6,7 @@ public class FirstPersonCameraControl : MonoBehaviour {
 
 	public Vector2 sensitivity;
 	public bool controlEnabled = false;
+	public bool startInFirstPerson;
 	public PhotographyManager cam;
 	
 //	private Mob mb;
@@ -14,6 +15,9 @@ public class FirstPersonCameraControl : MonoBehaviour {
 	void Start () {
 //		mb = GetComponent<Mob>();
 //		rbody = GetComponent<Rigidbody>();
+		if( startInFirstPerson ) {
+			changeToCam();
+		}
 	}
 	
 	// Update is called once per frame
@@ -31,16 +35,28 @@ public class FirstPersonCameraControl : MonoBehaviour {
 //			Debug.Log( Input.GetAxis("Mouse X") + ", " + Input.GetAxis("Mouse Y") );
 		}
 		if( Input.GetAxis( "Open Camera" ) > 0 ) {
-			if( controlEnabled ) {
-				controlEnabled = false;
-				GetComponent<OverheadPlayerControl>().controlEnabled = true;
-				cam.switchToGame();
-			} else {
-				controlEnabled = true;
-				GetComponent<OverheadPlayerControl>().controlEnabled = false;
-				cam.switchToPhoto();
+			if( controlEnabled && !cam.inTransition ) {
+				changeToGame();
+			} else if( !controlEnabled && !cam.inTransition ) {
+				changeToCam();
 			}
 		}
+	}
+
+	public void changeToGame() {
+		if( cam.inTransition )
+			return;
+		controlEnabled = false;
+		GetComponent<OverheadPlayerControl>().controlEnabled = true;
+		cam.switchToGame();
+	}
+
+	public void changeToCam() {
+		if( cam.inTransition )
+			return;
+		controlEnabled = true;
+		GetComponent<OverheadPlayerControl>().controlEnabled = false;
+		cam.switchToPhoto();
 	}
 }
 
