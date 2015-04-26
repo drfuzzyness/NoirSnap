c35081f0b1161fe8a9661b7575e4f184d79eb263
Matthew Conto <mbsconto@gmail.com>
2015-04-26 17:02:33 -0400

    What the actual fuck just happened when I switched branches

---
+++ Assets/Scripts/introDialogue.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class introDialogue : MonoBehaviour {
+	public Text text;
+	public Text text2;
+	public Text cont;
+	int count1 = 0;
+	int count2 = 0;
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.Space)){
+			count1 += 1;
+			count2 += 1;
+		}
+		if (count1 == 0){
+		
+		}
+		if (count1 == 1){
+		
+		}
+	}
+}
--- Assets/Scripts/pickupItem.cs
+++ Assets/Scripts/pickupItem.cs
@@ -5,58 +5,18 @@ using UnityEngine.UI;
 public class pickupItem : MonoBehaviour {
 	
 	Collider item;
-	Collider playerInside;
 	public Text text;
 	public static GameObject OnObject;
 
-	void Start(){
-		text.text = "";
-	}
-
-
-	void Update(){
-
-		if (playerInside != null){
-
-			text.text = "Press [E] to pick up item";
-			//FIND OUT HOW TO CHANGE ITEM NAME BASED ON ACTUAL ITEM STRING
-			//text.text = "Press [E] to pick up" + itemName;
-
-			if (Input.GetKeyDown(KeyCode.E)){
-
-				//Code where item is added to list player can access
-
-			}
-
-		}
-
-
-	}
 	void OnTriggerEnter(Collider other) {
-
-
-		if(other.tag == "Player")
-		{
-			text.text = "Press [E] to pick up item";
-			OnObject = other.gameObject;
-		}//FIND OUT HOW TO CHANGE ITEM NAME BASED ON ACTUAL ITEM STRING
-		//text.text = "Press [E] to pick up" + itemName
-
-		playerInside = other;
-
-
+		text.text = "Press [E] to pick up item";
+		//FIND OUT HOW TO CHANGE ITEM NAME BASED ON ACTUAL ITEM STRING
+		//text.text = "Press [E] to pick up" + itemName;
 		OnObject = other.gameObject;
-	
-
 	}
 	
-	void OnTriggerExit(Collider exiter) {
-		text.text = "";
-		playerInside = null;
-
-
+	void OnTriggerExit(Collider other) {
 		if (OnObject == this.gameObject) OnObject = null;
-
 	}
 
 }
