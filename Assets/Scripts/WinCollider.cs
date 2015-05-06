9da6c7a37abe9bc0bc32b33f87b25b52f8f6b64e
Shaun Bandrowski <shaun.toro@gmail.com>
2015-05-06 14:14:24 -0400

    Victory Condition and Instructions for Victory on Sewer Level

---
+++ Assets/Scripts/WinCollider.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WinCollider : MonoBehaviour {
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	void OnTriggerEnter(Collider activator) {
+
+		LevelManager.instance.playerWon();
+	}
+	
+	void OnTriggerExit(Collider exiter) {
+
+		
+		
+		//		if (OnObject == this.gameObject) OnObject = null;
+		
+	}
+
+}
